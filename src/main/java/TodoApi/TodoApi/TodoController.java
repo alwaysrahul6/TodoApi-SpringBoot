e16422c8dfaad5dbde801599d631c74f60015627
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-08-07 12:02:40 +0530

    Added todo cretaion

--- src/main/java/TodoApi/TodoApi/TodoController.java
+++ src/main/java/TodoApi/TodoApi/TodoController.java
@@ -1,5 +1,8 @@
 package TodoApi.TodoApi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -7,14 +10,27 @@ import java.util.List;
 @RestController
 public class TodoController {
 
-    private static List<Todo> todos;
+    private static List<Todo> todoList;
 
     TodoController(){
-        todos = new ArrayList<>();
-        todos.add(new Todo(1 , false , "Spring2" , 1));
-        todos.add(new Todo(2 , false , "System Design",2));
-        todos.add(new Todo(3 , false , "DSA" , 3));
-        todos.add(new Todo(4 , false , "OS" , 4));
+        todoList = new ArrayList<>();
+        todoList.add(new Todo(1 , false , "Spring2" , 1));
+        todoList.add(new Todo(2 , false , "System Design",2));
+        todoList.add(new Todo(3 , false , "DSA" , 3));
+        todoList.add(new Todo(4 , false , "OS" , 4));
+    }
+
+    @GetMapping("/todo")
+
+    public List<Todo> getTodo(){
+        return todoList;
+    }
+
+    @PostMapping("/todo")
+
+    public Todo createTodo(@RequestBody Todo newTodo){
+        todoList.add(newTodo);
+        return newTodo;
     }
 
 }
